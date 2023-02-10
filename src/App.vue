5cc86811dae8c774da92b7d0134924f751aed449
Prico3 <pricoco.andrea@gmail.com>
2023-02-10 15:41:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  ciao
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
