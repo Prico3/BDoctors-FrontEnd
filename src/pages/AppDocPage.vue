6b8797bb88f8bbafa4965fa052f876c18796029e
Mirko Robbi <mirko.robbi@gmail.com>
2023-02-14 18:12:42 +0100

    NEED TO: wait for api doctor

--- src/pages/AppDocPage.vue
+++ src/pages/AppDocPage.vue
@@ -1,12 +1,27 @@
 <script>
+import { store } from "../store";
 export default {
-    name: "AppDocPage"
-
+    name: "AppDocPage",
+    data() {
+        return {
+            store,
+            docData: [],
+        }
+    },
+    docPageApi() {
+        axios.get(`http://localhost:8000/api/doc/spec/${this.store.doctorId}`).then(resp => {
+            this.docData = resp.data[0];
+            console.log(this.docData);
+        })
+    },
+    created() {
+        this.docPageApi
+    }
 }
 </script>
 
 <template>
-    <h1>Ciao </h1>
+    <h1>{{ docData.name }} </h1>
 </template>
 
 <style>
