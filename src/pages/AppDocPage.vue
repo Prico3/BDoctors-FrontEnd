7b7b2da19ce7dd9ac581275d07bd6f2c8cee271e
Mirko Robbi <mirko.robbi@gmail.com>
2023-02-15 10:43:42 +0100

    RESOLVED: persistent dataloock

--- src/pages/AppDocPage.vue
+++ src/pages/AppDocPage.vue
@@ -7,25 +7,53 @@ export default {
         return {
             store,
             docData: [],
-            docSlug: ""
+            docSlug: "",
+            myData: "",
         }
     },
     methods: {
         docPageApi() {
-            axios.get(`http://localhost:8000/api/doc/${this.store.doctorSlug}`).then(resp => {
-                this.docData = resp.data.doctor
-                console.log(this.docData, "ciao");
-            })
+            if (this.store.doctorSlug !== "") {
+                axios.get(`http://localhost:8000/api/doc/${this.store.doctorSlug}`).then(resp => {
+                    this.docData = resp.data.doctor
+                    console.log(this.docData, "ciao");
+                })
 
+            } else {
+                console.log(this.myData, "my data else if");
+                axios.get(`http://localhost:8000/api/doc/${this.myData}`).then(resp => {
+                    this.docData = resp.data.doctor
+                    console.log(this.docData, "ciao");
+                })
+            }
         },
         slugCheck() {
             this.docData = this.store.docData
             console.log(this.docData)
+        },
+        redeemLocalSave() {
+            const savedData = localStorage.getItem('myData');
+            if (savedData !== null) {
+                console.log(savedData, "saved data");
+                this.myData = JSON.parse(savedData);
+                console.log(savedData, "my data?");
+                this.docPageApi()
+            }
+        },
+        localSave() {
+            console.log(this.store.doctorSlug, "local save");
+            if (this.myData == "") {
+                localStorage.setItem('myData', JSON.stringify(this.store.doctorSlug));
+            } else {
+                localStorage.setItem('myData', JSON.stringify(this.myData));
+            }
         }
     },
     created() {
-        this.docPageApi()
+        // this.docPageApi()
         this.slugCheck()
+        this.redeemLocalSave()
+        this.localSave()
     }
 }
 </script>
