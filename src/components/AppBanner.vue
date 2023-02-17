ac15fae089d1d75f8c2f25b6f9b8b900874c1004
Prico3 <pricoco.andrea@gmail.com>
2023-02-17 11:35:09 +0100

    Add: little changes

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -91,7 +91,7 @@ export default {
             }
         },
         nameSearch() {
-            debugger
+
             if (!this.searchDoc == "") {
                 this.newDocArray = []
                 this.docData.forEach(item => {
