bb658c9507b6d7bf2a07f938fdb2dddf426dac60
Prico3 <pricoco.andrea@gmail.com>
2023-02-20 11:52:55 +0100

    Add: adjust

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -36,50 +36,52 @@ export default {
 <template>
     <div class="row  g-4">
         <div class="text-center">
-            <!-- CARD -->
+            <div class="ms_div" :class="[doctor.sponsorized ? 'sponsored' : '']">
+                <!-- CARD -->
+                <div class="card">
+                    <!-- star - reviews  -->
+                    <p class="special ms_btn px-2" href="">
+                        <i class="fa-solid fa-star"></i>
+                        {{ doctor.mediaVote }}
+                    </p>
 
-            <div class="card" :class="[doctor.sponsorized ? 'sponsored' : '' ]">
-                <!-- star - reviews  -->
-                <p class="special ms_btn px-2" href="">
-                    <i class="fa-solid fa-star"></i>
-                    {{ doctor.mediaVote }}
-                </p>
+                    <p class="special-2 ms_btn-2 px-2" href="">
+                        <i class="fa-solid fa-comment-dots"></i>
+                        {{ doctor.numReviews }}
+                    </p>
+                    <!-- <span>{{ doctor.numReviews }}</span> -->
+                    <div class="img-wrapper">
+                        <!-- profile photo -->
+                        <img v-if="doctor.photo" :src="`http://localhost:8000/storage/${doctor.photo}`" alt="">
+                        <img v-else src="../assets/img/doctor-1-removebg-preview.png" alt="">
+                    </div>
 
-                <p class="special-2 ms_btn-2 px-2" href="">
-                    <i class="fa-solid fa-comment-dots"></i>
-                    {{ doctor.numReviews }}
-                </p>
-                <!-- <span>{{ doctor.numReviews }}</span> -->
-                <div class="img-wrapper">
-                    <!-- profile photo -->
-                    <img v-if="doctor.photo" :src="`http://localhost:8000/storage/${doctor.photo}`" alt="">
-                    <img v-else src="../assets/img/doctor-1-removebg-preview.png" alt="">
+                    <div class="card-body">
+                        <!-- doctor name -->
+                        <h5 class="card-title">{{ doctor.name }} {{ doctor.surname }}</h5>
+                        <!-- doctor address -->
+                        <p class="card-text">{{ doctor.studio_address }}</p>
+                    </div>
+
+                    <ul class="list-group list-group-flush">
+                        <!-- specialization -->
+                        <li class="list-group-item">{{ doctor.specialization }}</li>
+
+                    </ul>
+
+                    <div class="card-body">
+                        <!-- send message -->
+                        <!-- <a href="#" class="card-link">Send a Message</a> -->
+                        <!-- send reviews -->
+                        <!-- <a href="#" class="card-link">Send Reviews</a> -->
+                        <!-- see more details -->
+                        <router-link to="doc" @click="showDoc" class="card-link btn btn-primary docPage">See
+                            details</router-link>
+                    </div>
                 </div>
-
-                <div class="card-body">
-                    <!-- doctor name -->
-                    <h5 class="card-title">{{ doctor.name }} {{ doctor.surname }}</h5>
-                    <!-- doctor address -->
-                    <p class="card-text">{{ doctor.studio_address }}</p>
-                </div>
-
-                <ul class="list-group list-group-flush">
-                    <!-- specialization -->
-                    <li class="list-group-item">{{ doctor.specialization }}</li>
-
-                </ul>
-
-                <div class="card-body">
-                    <!-- send message -->
-                    <!-- <a href="#" class="card-link">Send a Message</a> -->
-                    <!-- send reviews -->
-                    <!-- <a href="#" class="card-link">Send Reviews</a> -->
-                    <!-- see more details -->
-                    <router-link to="doc" @click="showDoc" class="card-link btn btn-primary docPage">See
-                        details</router-link>
-                </div>
+                <!-- /CARD -->
             </div>
-            <!-- /CARD -->
+
 
         </div>
 
@@ -139,6 +141,9 @@ a {
         transition: 2s;
     }
 
+    border: none;
+    border-radius: 10px;
+
     // &.div-photo {
     //     max-width: 200px;
     // }
@@ -146,8 +151,11 @@ a {
 
 .sponsored {
 
-    background-color: gold;
+    border: 20px solid rgb(255, 215, 0, 0.8);
+    border-radius: 15px;
+    background-color: rgb(255, 215, 0, 0.8);
+
+
 
 }
-
 </style>
