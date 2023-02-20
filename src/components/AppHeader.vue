56a69db696d49d0cb7ca766c0a1f604694101fa6
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2023-02-20 13:02:46 +0100

    ADD FIX: Hamburger menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -30,53 +30,34 @@ export default {
 <template>
     <header>
         <!-- NAVBAR -->
-        <nav class="navbar  navbar-expand-lg bg">
-
-            <!-- LOGO -->
+        <nav class="navbar navbar-expand-lg bg">
             <div class="container-fluid">
                 <div class="d-flex align-items-center">
-
                     <img src="../assets/img/logo-removebg-preview.png" alt="">
                     <router-link to="/" class="navbar-brand text-light fs-1 hvr" href="#">BDoctors</router-link>
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav">
+                            <li class="nav-item me-4">
+                                <a href="http://localhost:8000" class="btn btn-light text-primary">Login</a>
+                            </li>
+                            <li class="nav-item me-4">
+                                <a href="http://localhost:8000/register" class="btn btn-light text-primary">Register</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <!-- LOGO -->
-
-
-            <!-- <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item" v-for="(navItem, index) in navigation" :key="index">
-                        <router-link :to="{ name: navItem.routeName }" class="nav-link" aria-current="page">{{
-                            navItem.label
-                        }}</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="http://localhost:8000/admin">Login</a>
-                    </li>
-                </ul>
-            </div> -->
-
-            <!-- BUTTONS -->
-            <div class="collapse navbar-collapse " id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item me-4">
-                        <a href="http://localhost:8000" class="btn btn-light text-primary">Login</a>
-                    </li>
-                    <li class="nav-item me-4">
-                        <a href="http://localhost:8000/register" class="btn btn-light text-primary">Register</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /BUTTONS -->
-
-
         </nav>
-        <!-- /NAVBAR -->
+
+
+
 
     </header>
 </template>
